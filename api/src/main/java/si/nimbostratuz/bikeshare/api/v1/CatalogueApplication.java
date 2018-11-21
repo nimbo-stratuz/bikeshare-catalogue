99d7ee1b5982e452a0ddffde1ffeab57b5539353
Matevž Fabjančič <mf6422@student.uni-lj.si>
2018-11-21 09:08:58 +0100

    Fix @RegisterService

--- api/src/main/java/si/nimbostratuz/bikeshare/api/v1/CatalogueApplication.java
+++ api/src/main/java/si/nimbostratuz/bikeshare/api/v1/CatalogueApplication.java
@@ -5,7 +5,7 @@ import com.kumuluz.ee.discovery.annotations.RegisterService;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@RegisterService
+@RegisterService("bikeshare-catalogue")
 @ApplicationPath("v1")
 public class CatalogueApplication extends Application {
 }
