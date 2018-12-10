08638b5387dfe55e615ce5a6753ecd9b12abab0d
Matevž Fabjančič <mf6422@student.uni-lj.si>
2018-12-10 19:16:08 +0100

    Log generated X-Request-ID

--- services/src/main/java/si/nimbostratuz/bikeshare/services/producers/RequestIdProducer.java
+++ services/src/main/java/si/nimbostratuz/bikeshare/services/producers/RequestIdProducer.java
@@ -1,5 +1,7 @@
 package si.nimbostratuz.bikeshare.services.producers;
 
+import com.kumuluz.ee.logs.LogManager;
+import com.kumuluz.ee.logs.Logger;
 import si.nimbostratuz.bikeshare.models.common.RequestId;
 
 import javax.enterprise.context.RequestScoped;
@@ -9,6 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.UUID;
 
 public class RequestIdProducer {
+
+    private Logger log = LogManager.getLogger(RequestIdProducer.class.getName());
 
     @Inject
     private HttpServletRequest request;
@@ -24,6 +28,7 @@ public class RequestIdProducer {
         if (currentRequestId != null) {
             requestId.set(currentRequestId);
         } else {
+            log.debug("No X-Request-ID on request. Generating new UUID.");
             requestId.set(UUID.randomUUID().toString());
         }
 
