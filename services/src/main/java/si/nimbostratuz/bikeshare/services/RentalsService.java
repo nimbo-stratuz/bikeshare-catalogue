11d2eb778388ef38ba0dbfd9ebfb641790475b81
Matevž Fabjančič <mf6422@student.uni-lj.si>
2018-12-11 12:52:05 +0100

    CircuitBreaker

--- services/src/main/java/si/nimbostratuz/bikeshare/services/RentalsService.java
+++ services/src/main/java/si/nimbostratuz/bikeshare/services/RentalsService.java
@@ -6,19 +6,16 @@ import com.kumuluz.ee.logs.Logger;
 import com.kumuluz.ee.logs.cdi.Log;
 import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
 import org.eclipse.microprofile.faulttolerance.Fallback;
-import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.metrics.annotation.Metered;
 import org.glassfish.jersey.client.ClientProperties;
 import si.nimbostratuz.bikeshare.models.common.RequestId;
 import si.nimbostratuz.bikeshare.models.dtos.RentalDTO;
+import si.nimbostratuz.bikeshare.services.configuration.BikeshareConfig;
 
-import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.ws.rs.ProcessingException;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.GenericType;
-import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Optional;
 
@@ -34,37 +31,33 @@ public class RentalsService {
     private Optional<WebTarget> rentalWebTarget;
 
     @Inject
+    private BikeshareConfig config;
+
+    @Inject
     private RequestId requestId;
 
-    @PostConstruct
-    public void init() {
-        this.rentalWebTarget = rentalWebTarget.map(
-                webTarget -> webTarget.property(ClientProperties.CONNECT_TIMEOUT, 500)
-                                      .property(ClientProperties.READ_TIMEOUT, 500));
-    }
 
     @Metered
     @CircuitBreaker(requestVolumeThreshold = 3)
-    @Timeout(value = 500L, unit = ChronoUnit.MILLIS)
     @Fallback(fallbackMethod = "getLastRentalsForBicycleFallback")
     public Optional<List<RentalDTO>> getLastRentalsForBicycle(Integer bicycleId, Integer limit) {
+
+        log.info("Using main method for {}.getLastRentalsForBicycle", RentalsService.class.getSimpleName());
 
         log.debug("rentalWebTarget: {}", rentalWebTarget.map(wt -> wt.getUri().toString())
                                                         .orElse("None"));
 
         if (rentalWebTarget.isPresent()) {
-            try {
-                return Optional.of(rentalWebTarget.get().path("v1")
-                                                  .path("rentals")
-                                                  .queryParam("where", "bicycleId:EQ:" + bicycleId)
-                                                  .queryParam("limit", limit)
-                                                  .queryParam("order", "rentStart DESC")
-                                                  .request()
-                                                  .header("X-Request-ID", requestId.get())
-                                                  .get(new GenericType<List<RentalDTO>>() {}));
-            } catch (ProcessingException e) {
-                log.error("getLastRentalsForBicycle", e);
-            }
+            return Optional.of(rentalWebTarget.get().path("v1")
+                                              .path("rentals")
+                                              .queryParam("where", "bicycleId:EQ:" + bicycleId)
+                                              .queryParam("limit", limit)
+                                              .queryParam("order", "rentStart DESC")
+                                              .request()
+                                              .header("X-Request-ID", requestId.get())
+                                              .property(ClientProperties.CONNECT_TIMEOUT, config.getRentalsServiceTimeout())
+                                              .property(ClientProperties.READ_TIMEOUT, config.getRentalsServiceTimeout())
+                                              .get(new GenericType<List<RentalDTO>>() {}));
         } else {
             log.warn("bikeshare-rentals cannot be discovered");
         }
@@ -73,6 +66,7 @@ public class RentalsService {
     }
 
     public Optional<List<RentalDTO>> getLastRentalsForBicycleFallback(Integer bicycleId, Integer limit) {
+        log.info("Using fallback for {}.getLastRentalsForBicycle", RentalsService.class.getSimpleName());
         return Optional.empty();
     }
 }
